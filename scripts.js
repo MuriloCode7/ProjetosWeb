9d55dcdd6c1dc2acf4cfa91a8fcb41e00493bf90
MuriloMagal <101419493+MuriloMagal@users.noreply.github.com>
2023-04-21 02:24:39 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var formAddCarrinho = document.getElementById('formAddCarrinho')
+
+formAddCarrinho.addEventListener('submit', function(e){
+    alert("Produto adicionado ao carrinho!")
+    e.preventDefault()
+})
+
