4f85d4189f1af1fedecf52ea696ec5b0a9b53752
MuriloMagal <murilomagalhaes60@gmail.com>
2023-08-31 10:29:21 -0300

    Delete trash

--- README.md
+++
@@ -1,6 +0,0 @@
-# ProjetosWeb
-Sites Desenvolvidos: 
-
-> RedTex: Venda de mangás (HQ's)
-
-- O projeto se encontra hospedado no drive devido ao tamanho
--- scripts.js
+++
@@ -1,7 +0,0 @@
-var formAddCarrinho = document.getElementById('formAddCarrinho')
-
-formAddCarrinho.addEventListener('submit', function(e){
-    alert("Produto adicionado ao carrinho!")
-    e.preventDefault()
-})
-
